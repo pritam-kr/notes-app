45ab65e69f93b1d876b18b6e6aa3d18df05fb10a
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-07-01 12:24:31 +0530

    Done

--- script.js
+++ script.js
@@ -1,61 +1,68 @@
 const newNote = document.querySelector(".new-notes");
 
+const showNotes = JSON.parse(localStorage.getItem('notes'));
+
+if (showNotes) {
+    showNotes.forEach((note) => {
+        addNewNote(note)
+    })
+}
+
 newNote.addEventListener("click", () => addNewNote());
 
 function addNewNote(text = "") {
-  const notBox = document.createElement("div");
-  notBox.classList.add("note");
-  notBox.innerHTML = `
+
+    const notBox = document.createElement("div");
+    notBox.classList.add("note");
+    notBox.innerHTML = `
     
                 <div class="tools">
                     <button class="btn-edit"><i class="far fa-edit"></i></button>
                     <button class="btn-delete"><i class="fas fa-trash-alt"></i></button>
                 </div>
 
-                <div class="main ${text ? "hidden": ""}"></div>
-                <textarea class="textarea ${text ? "" : "hidden"}"></textarea>
+                <div class="main ${text ? "" : "hidden"}"></div>
+                <textarea class=" ${text ? "hidden" : ""}"></textarea>
 
     `;
 
-  const delBtn = notBox.querySelector(".btn-delete");
-  delBtn.addEventListener("click", () => {
-    notBox.remove();
-    mylocalStorage();
-  });
-  const editBtn = notBox.querySelector(".btn-edit");
-  const main = notBox.querySelector(".main");
-  const textArea = notBox.querySelector(".textarea");
-  editBtn.addEventListener("click", () => {
-    main.classList.toggle("hidden");
-    textArea.classList.toggle("hidden");
-  });
+    const delBtn = notBox.querySelector(".btn-delete");
+    delBtn.addEventListener("click", () => {
+        notBox.remove();
+        mylocalStorage();
+    });
 
-  textArea.addEventListener("input", (e) => {
-    const { value } = e.target;
-    main.innerHTML = marked(value);
-    mylocalStorage();
-  });
+    const editBtn = notBox.querySelector(".btn-edit");
+    const main = notBox.querySelector(".main");
+    const textArea = notBox.querySelector("textarea");
 
-  document.querySelector(".notes-container").appendChild(notBox);
+    textArea.value =  text
+    main.innerHTML = marked(text)
+
+    editBtn.addEventListener("click", () => {
+        main.classList.toggle("hidden")
+        textArea.classList.toggle("hidden")
+    });
+
+    textArea.addEventListener("input", (e) => {
+        const { value } = e.target;
+        main.innerHTML = marked(value);
+        mylocalStorage();
+    });
+
+    document.querySelector(".notes-container").appendChild(notBox);
 }
 
 //local storage
 
 function mylocalStorage() {
-  const allTextArea = document.querySelectorAll("textarea");
-  const myNotes = [];
+    const allTextArea = document.querySelectorAll("textarea");
+    const myNotes = [];
 
-  allTextArea.forEach((el) => {
-    myNotes.push(el.value);
-  });
+    allTextArea.forEach((el) => {
+        myNotes.push(el.value);
+    });
 
-  localStorage.setItem('notes', JSON.stringify(myNotes));
+    localStorage.setItem('notes', JSON.stringify(myNotes));
 }
 
-const showNotes = JSON.parse(localStorage.getItem('notes'));
-
-if(showNotes){
-    showNotes.forEach((note) => {
-     addNewNote(note)
-    })
-}
